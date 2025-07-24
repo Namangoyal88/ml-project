2a7c13ceca3e5fb1c8806f3c2cc4203500f6e304
Naman Goyal <goyalnaman497gmail.com>
2025-07-25 00:15:31 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,7 +41,36 @@ class ModelTrainer:
                 "XGBoost Regressor": XGBRegressor()
             }
             
-            model_report:dict = evaluate_model(x_train, y_train, x_test, y_test, models)
+            params = {
+                "Linear Regression": {},
+                "Decision Tree Regressor": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest Regressor": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "XGBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoost Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "KNeighbors Regressor": {}
+            }
+            
+            model_report:dict = evaluate_model(x_train, y_train, x_test, y_test, models, params)
                 
             best_model_score = max(model_report.values())
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
